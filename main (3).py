87a2ae9b7aeed1dc05d4412b0085c3405968b0aa
Theuzin <mateusetec424@gmail.com>
2025-03-26 16:20:31 -0300

    Numero maior ou menor com float (3).py

--- main (3).py
+++ main (3).py
@@ -1,3 +1,4 @@
+# Código utilizando float e função if e else, para saber se um numero e maior ou menor
 num1 = float (input("Digite o primeiro numero"));
 num2 = float (input("Digite o segundo numero"));
 if num1 > num2:
