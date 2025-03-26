5933996647d234c662d77f47e08afb3aa6e79451
Theuzin <mateusetec424@gmail.com>
2025-03-26 16:18:34 -0300

    Tabuada main (5).py

--- main (5).py
+++ main (5).py
@@ -1,3 +1,4 @@
+# Tabuada de nqualquer numéro de 1 a 10
 tabuada = int (input("Tabuada desse numero é: "));
 for count in range(10):
     print("%d x %d = %d" % (tabuada, count+1, tabuada*(count+1)))
