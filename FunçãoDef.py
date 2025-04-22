a5def04892585a02e6de5a8e8380358884b028a8
Theuzin <mateusetec424@gmail.com>
2025-04-22 20:56:54 -0300

    Update and rename main.py to FunçãoDef.py

---
+++ FunçãoDef.py
@@ -0,0 +1,32 @@
+#Calculo da velocidade media
+import sys
+
+def calculaVelocidadeMedia(distancia, tempo):
+    return distancia / tempo
+    
+
+def main():
+    d = float(input("Informe a distancia para chegar no seu destino: "))
+    t = int(input("Informe quantas horas você tem para chegar: "))
+    velocidadeMedia = calculaVelocidadeMedia(d, t)
+    print(f"A velocidade média para chegar no destino é {velocidadeMedia} Km por hora.")
+
+if __name__=="__main__":
+    main()
+
+#Calculo do numero fatorial
+import sys
+import math
+
+def valorFatorial(num):
+    return math.factorial(valorFatorial)
+    
+def main():
+    num = int(input('Digite um valor'))
+    fatorial = math.factorial(num)
+    print('O valor fatorial desse numero é: {}'.format(fatorial))
+
+if __name__=='__main__':
+    main()
+
+
--- main.py
+++
@@ -1,5 +0,0 @@
-# Média Aritmética
-num1 = float(input("Digite o primeiro valor"))
-num2 = float(input("Digite o segundo valor"))
-soma = (num1 + num2) /2
-print(f"A media aritmética é, {soma}")
