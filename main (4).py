198e0348e3d0a095faf70552d8dc632c8f7b6b1f
Theuzin <mateusetec424@gmail.com>
2025-04-23 19:48:10 -0300

    Delete main (4).py

--- main (4).py
+++
@@ -1,8 +0,0 @@
-# Código utilizando função if para saber se o valor e positivo, negativo ou igual a zero
-num1 = int (input("Digite um valor"));
-if num1 > 0:
-    print("O numero e positivo")
-if num1 == 0:
-    print("O valor e zero")
-if num1 < 0:
-    print('O valor e negativo')
