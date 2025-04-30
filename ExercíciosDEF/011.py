cf01d17c38312900a0dc8457ceb2ec1c7bd66957
Theuzin <mateusetec424@gmail.com>
2025-04-30 09:14:29 -0300

    Update 011.py

--- ExercíciosDEF/011.py
+++ ExercíciosDEF/011.py
@@ -1,4 +1,15 @@
+#Sequência de Fibonacci
 
+def fibonacci_recursive(n):
+    
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+num_terms = 10
+for i in range(num_terms):
+    print(fibonacci_recursive(i), end=" ")
 
 
 
