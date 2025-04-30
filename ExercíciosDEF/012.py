9a5766aa4f07ae9284e2b600f40c640b4b793bec
Theuzin <mateusetec424@gmail.com>
2025-04-30 09:43:45 -0300

    Update 012.py

--- ExercíciosDEF/012.py
+++ ExercíciosDEF/012.py
@@ -1,1 +1,28 @@
+#Numeros Primos
+
+import sys
+
+def ehPrimo(numero):
+    if type(numero) != int:
+        return False
+    if numero <= 2:
+        return True
+
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+
+        return True
+
+def main():
+
+    valor = int(input('Digite um valor: '))
+    if ehPrimo(valor):
+        print('O numero {} é primo.'.format(valor))
+    else:
+        print('O numero {} não é primo.'.format(valor))
+
+
+
+
 
