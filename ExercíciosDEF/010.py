d87d11246691cacf006d810683437d23325681fb
Theuzin <mateusetec424@gmail.com>
2025-04-30 09:13:11 -0300

    Update 010.py

--- ExercíciosDEF/010.py
+++ ExercíciosDEF/010.py
@@ -1,1 +1,18 @@
+#Fatorial de um número
+
+import sys
+import math
+
+def valorFatorial(num):
+    return math.factorial(valorFatorial)
+    
+def main():
+    num = int(input('Digite um valor'))
+    fatorial = math.factorial(num)
+    print('O valor fatorial desse numero é: {}'.format(fatorial))
+
+if __name__=='__main__':
+    main()
+
+
 
