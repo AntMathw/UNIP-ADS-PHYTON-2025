d96f4711a73f67dff8d555828265424bc3cf006c
Theuzin <mateusetec424@gmail.com>
2025-04-30 10:35:49 -0300

    Update 017.py

--- ExercíciosDEF/017.py
+++ ExercíciosDEF/017.py
@@ -13,10 +13,16 @@ def palindromo(string):
             return False
         return True
 
+def inverter_frase(frase):
+    return frase[::-1]
+
 def main():
     frase = input('Digite uma frase: ')
+    frase_invertida = inverter_frase(frase)
     if palindromo(frase):
-        print('Sua frase em palindromo {}'.format(frase))
+        print('A frase digitada invertida é {}'.format(frase_invertida))
+        if frase.lower() == frase_invertida.lower().replace(' ', ' '):
+            print('Sua frase é um palindromo')
     else:
         print('Não é um palindromo')
 
