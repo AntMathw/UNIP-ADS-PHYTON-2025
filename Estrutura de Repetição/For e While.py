8bace5bf380d8ff90de9775290a12c87f022b912
Theuzin <mateusetec424@gmail.com>
2025-05-10 15:36:18 -0300

    Update For e While.py

--- Estrutura de Repetição/For e While.py
+++ Estrutura de Repetição/For e While.py
@@ -154,3 +154,38 @@ elif totalmulheres == 1:
 else:
     print('No total, existem {} mulheres com menos de 20 anos.'.format(totalmulheres))
   
+#Jogo do Par ou Ímpar
+
+from random import randint
+
+print('--'*30)   
+print('JOGO DO PAR E ÍMPAR')
+print('--'*30)
+v = 0
+
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 11)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PpIi':
+        tipo = str(input('Par ou ímpar? ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}', end='')
+    print(', deu par!' if total % 2 == 0 else ', deu ímpar!')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Parabéns, você ganhou!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Parabéns, você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    print('Vamos jogar novamente.')
+print(f'Fim de jogo! Você venceu {v} vezes!')
+
