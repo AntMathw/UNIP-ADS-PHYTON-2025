c3250158a50fbdd0d59701e8b1a9d38a08a878c3
Theuzin <mateusetec424@gmail.com>
2025-05-13 15:19:45 -0300

    Update If, Elif e Else.py

--- Estrutura Condicional/If, Elif e Else.py
+++ Estrutura Condicional/If, Elif e Else.py
@@ -218,9 +218,65 @@ else:
         if computador == 3:
             print('EMPATE')
 
+#Conta de Banco
+
+conta_normal = False
+conta_universitaria = False
+conta_especial = True
+
+saldo = 2000
+saque = 1500
+cheque_especial = 450
+
+if conta_normal:
+
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial!")
+    else:
+        print("Não foi possivel realizar o saque, saldo insuficiente!")
+
+elif conta_universitaria:
+
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
+
+elif conta_especial:
+    print("Conta especial selecionada!")
+
+else:
+    print("Sistema não reconheceu seu tipo de conta, entre em contato com o seu gerente.")
 
 
+#Saber se é maior de idade, em formato que a variavel e intangivel
 
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CHN.")
+
+if idade < MAIOR_IDADE:
+    print("Ainda não pode tirar a CNH.")
+
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CHN.")
+else:
+    print("Ainda não pode tirar a CNH.")
+
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CHN.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
+else:
+    print("Ainda não pode tirar a CNH.")
 
 
 
