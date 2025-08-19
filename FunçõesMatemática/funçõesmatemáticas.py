772d0c74f8bc28ba75783c3808bf5ca927814352
Theuzin <mateusetec424@gmail.com>
2025-08-19 15:46:13 -0300

    Update funçõesmatemáticas.py

--- FunçõesMatemática/funçõesmatemáticas.py
+++ FunçõesMatemática/funçõesmatemáticas.py
@@ -1,1 +1,38 @@
+#Tipos primitivos
+
+valor = float(input('Digite um valor: '))
+n = float(input('Digite um valor real: '))
+
+soma = valor + n
+print('A some entre {} e {} é {}'.format(valor, n, soma))
+
+valor2 = float(input('Digite um numero real: '))
+n2 = float(input('Digite outro valor: '))
+
+divisão = valor2 / n2
+
+print('A divisão entre {} e {} é {}'.format(valor2, n2, divisão))
+
+num1 = float(input('Digite um valor real: '))
+num2 = float(input('Digite outro numero real: '))
+
+multiplicação = num1 * num2
+
+print('A multiplicação entre {} e {} é {}'.format(num1, num2, multiplicação))
+
+
+#Operaores Artiméticos
+
+num = int(input('Digite um valor: '))
+num2 = 2
+
+valor = num + 2
+valor2 = num - 2
+valor3 = num * 2
+valor4 = num / 2
+valor5 = num ** 2
+valor6 = num // 2
+valor7 = num % 2
+
+print('Os valores são \n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(valor, valor2, valor3, valor4, valor5, valor6, valor7))
 
