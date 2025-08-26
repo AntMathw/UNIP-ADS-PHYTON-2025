b138225a71572e801f13637138ec3416d67285b0
Theuzin <mateusetec424@gmail.com>
2025-08-25 21:23:56 -0300

    Update Pratica de Codificação ESTUDOS.py

--- Pratica de Codificação ESTUDOS.py
+++ Pratica de Codificação ESTUDOS.py
@@ -377,3 +377,19 @@ num = int(input('Digite um número e mostra sua tabuada: '))
 while cont < 10:
     cont += 1
     print(f'{num} x {cont} = {num * cont}')
+
+#Contatos em JSON
+
+arquivo_contatos = 'contatos.json'
+
+def carregar_contatos():
+    if os.path.exists(arquivo_contatos):
+        try:
+            with open(arquivo_contatos, 'r', encoding='utf-8')as arquivo:
+                return json.load(arquivo)
+            
+        except json.JSONDecodeError:
+            print('Erro ao carregar arquivo.')
+            return()
+        
+    return()
