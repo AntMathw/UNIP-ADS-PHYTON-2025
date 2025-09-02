975c9703a6108041439cfc2dd5d5e9f9351613c8
Theuzin <mateusetec424@gmail.com>
2025-09-02 19:37:48 -0300

    Update práticas.py

--- ExercíciosDEF/práticas.py
+++ ExercíciosDEF/práticas.py
@@ -12,3 +12,19 @@ def main():
     print('A média do aluno foi {:.2f}'.format(media))
 
 main()
+
+#Somatório de dois valores
+
+def soma(num1, num2):
+    somatorio = num1 + num2
+    return somatorio
+
+def main():
+    num1 = int(input('Digite um valor: '))
+    num2 = int(input("Digite outro valor: "))
+    resultado = soma(num1, num2)
+    print(f'A soma entre {num1} + {num2} = {resultado}')
+
+main()
+
+
