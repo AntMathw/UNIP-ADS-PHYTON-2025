ca6196ec717def5acc95f24d52b0427462ec41d6
Theuzin <mateusetec424@gmail.com>
2025-09-17 21:37:47 -0300

    Update Conteúdo.py

--- Pilha/Conteúdo.py
+++ Pilha/Conteúdo.py
@@ -25,3 +25,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#Exemplo de prática do LIFO(Last In First Out)
+
+# LIFO (Último a Entrar, Primeiro a Sair) usando Pilha
+class Pilha:
+    def __init__(self):
+        self.pilha = []
+    
+    def empurrar(self, item):
+        self.pilha.append(item)
+        print(f"Item empurrado: {item}")
+    
+    def remover(self):
+        if self.esta_vazia():
+            return "Pilha está vazia"
+        return self.pilha.pop()
+    
+    def topo(self):
+        if self.esta_vazia():
+            return "Pilha está vazia"
+        return self.pilha[-1]
+    
+    def esta_vazia(self):
+        return len(self.pilha) == 0
+    
+    def tamanho(self):
+        return len(self.pilha)
