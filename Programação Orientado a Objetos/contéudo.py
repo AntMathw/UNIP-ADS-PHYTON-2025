3c3f8387b169b175e69b0f9b7ebdcbc600e290ae
Theuzin <mateusetec424@gmail.com>
2025-09-29 21:42:35 -0300

    Update contéudo.py

--- Programação Orientado a Objetos/contéudo.py
+++ Programação Orientado a Objetos/contéudo.py
@@ -1,1 +1,21 @@
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+    def emitir_som(self):
+        pass
+class Cachorro(Animal):
+    def emitir_som(self):
+        return 'AU, AU!'
+
+class Gato(Animal):
+    def emitir_som(self):
+        return 'MIAU!'
+
+dog = cachorro('Ronaldo')
+cat = gato('Estrassalha Pussy')
+
+print(dog.nome,'-',dog.emitir_som())
+print(cat.nome,'-',cat.emitir_som())
+
+
 
